9c7e66cadb8d82a43dead81cea5809365fb47cb2
ykropchik <ykropchik22@gmail.com>
2022-06-08 10:06:17 +1000

    FE-2 Добавил стабы пользователей

---
+++ web/stubs/users.js
@@ -0,0 +1,162 @@
+export const Users = [
+	{
+		name: 'Jimenez Jones',
+		avatarUrl: null,
+		email: 'jimenezjones@enersol.com',
+		specialization: 'DevOps',
+		grade: 'Junior',
+		role: 'Lider'
+	},
+	{
+		name: 'Lolita House',
+		avatarUrl: null,
+		email: 'lolitahouse@enersol.com',
+		specialization: 'DevOps',
+		grade: 'Junior',
+		role: 'Lider'
+	},
+	{
+		name: 'Diana Noel',
+		avatarUrl: 'https://i.pravatar.cc/100',
+		email: 'diananoel@enersol.com',
+		specialization: 'IOS',
+		grade: 'Midle',
+		role: 'Scrum'
+	},
+	{
+		name: 'Sweeney Robertson',
+		avatarUrl: null,
+		email: 'sweeneyrobertson@enersol.com',
+		specialization: 'IOS',
+		grade: 'Midle',
+		role: 'Scrum'
+	},
+	{
+		name: 'Fox Tyler',
+		avatarUrl: null,
+		email: 'foxtyler@enersol.com',
+		specialization: 'Frontend',
+		grade: 'Junior',
+		role: 'Default'
+	},
+	{
+		name: 'Combs Williamson',
+		avatarUrl: null,
+		email: 'combswilliamson@enersol.com',
+		specialization: 'Java',
+		grade: 'Senior',
+		role: 'Default'
+	},
+	{
+		name: 'Petersen Brooks',
+		avatarUrl: null,
+		email: 'petersenbrooks@enersol.com',
+		specialization: 'Android',
+		grade: 'Junior',
+		role: 'Lider'
+	},
+	{
+		name: 'Moon Morrison',
+		avatarUrl: null,
+		email: 'moonmorrison@enersol.com',
+		specialization: 'Frontend',
+		grade: 'Midle',
+		role: 'Default'
+	},
+	{
+		name: 'Tamra Watson',
+		avatarUrl: null,
+		email: 'tamrawatson@enersol.com',
+		specialization: 'Android',
+		grade: 'Junior',
+		role: 'Default'
+	},
+	{
+		name: 'Hatfield Ayers',
+		avatarUrl: null,
+		email: 'hatfieldayers@enersol.com',
+		specialization: 'Android',
+		grade: 'Junior',
+		role: 'Default'
+	},
+	{
+		name: 'Debbie Talley',
+		avatarUrl: null,
+		email: 'debbietalley@enersol.com',
+		specialization: 'IOS',
+		grade: 'Junior',
+		role: 'Default'
+	},
+	{
+		name: 'Esther Shelton',
+		avatarUrl: null,
+		email: 'esthershelton@enersol.com',
+		specialization: 'DevOps',
+		grade: 'Junior',
+		role: 'Default'
+	},
+	{
+		name: 'Frank Anthony',
+		avatarUrl: null,
+		email: 'frankanthony@enersol.com',
+		specialization: 'IOS',
+		grade: 'Senior',
+		role: 'Lider'
+	},
+	{
+		name: 'Jeannette Mckenzie',
+		avatarUrl: 'https://i.pravatar.cc/100',
+		email: 'jeannettemckenzie@enersol.com',
+		specialization: 'IOS',
+		grade: 'Senior',
+		role: 'Scrum'
+	},
+	{
+		name: 'Rosemarie Santana',
+		avatarUrl: null,
+		email: 'rosemariesantana@enersol.com',
+		specialization: 'Frontend',
+		grade: 'Senior',
+		role: 'Default'
+	},
+	{
+		name: 'Ella Valentine',
+		avatarUrl: null,
+		email: 'ellavalentine@enersol.com',
+		specialization: 'IOS',
+		grade: 'Midle',
+		role: 'Scrum'
+	},
+	{
+		name: 'Brady Tillman',
+		avatarUrl: 'https://i.pravatar.cc/100',
+		email: 'bradytillman@enersol.com',
+		specialization: 'Frontend',
+		grade: 'Junior',
+		role: 'Scrum'
+	},
+	{
+		name: 'Avila Brock',
+		avatarUrl: 'https://i.pravatar.cc/100',
+		email: 'avilabrock@enersol.com',
+		specialization: 'IOS',
+		grade: 'Midle',
+		role: 'Scrum'
+	},
+	{
+		name: 'Mooney Townsend',
+		avatarUrl: null,
+		email: 'mooneytownsend@enersol.com',
+		specialization: 'Frontend',
+		grade: 'Senior',
+		role: 'Lider'
+	},
+	{
+		name: 'Dixie Boone',
+		avatarUrl: null,
+		email: 'dixieboone@enersol.com',
+		specialization: 'DevOps',
+		grade: 'Midle',
+		role: 'Scrum'
+	}
+];
