f6eaf6961e9e1c6b59e73f20e950830d5ccc0f25
neu_net <przman2000@mail.ru>
2022-06-11 21:04:40 +1000

    BE-3 Сделал контроллер грейдов

---
+++ api/src/Controller/GradeController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Grade;
+use App\Repository\GradeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GradeController extends AppController
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/api/grade', name: 'add_grade', methods: ['POST'])]
+    public function add_grade(Request $request): Response
+    {
+        try {
+            $request = $this->transformJsonBody($request);
+
+            $grade = new Grade();
+            $grade->setName($request->get('name'));
+            $grade->setDescription($request->get('description'));
+
+            $this->entityManager->persist($grade);
+            $this->entityManager->flush();
+
+            $data = [
+                'status' => Response::HTTP_OK,
+                'success' => 'Grade added successfully',
+            ];
+
+            return $this->response($data);
+        } catch (\Exception $e) {
+            $data = [
+                'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                'errors' => $e->getMessage(),
+            ];
+            return $this->response($data, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    #[Route('/api/grade', name: 'get_all_grades', methods: ['GET'])]
+    public function get_grades(GradeRepository $gradeRepository): Response
+    {
+        try {
+            $grades = $gradeRepository->findAll();
+
+            $gradeArray = [];
+
+            foreach ($grades as $grade) {
+                $temp_grade = [
+                    'id' => $grade->getId(),
+                    'name' => $grade->getName(),
+                    'description' => $grade->getDescription(),
+                ];
+                array_push($gradeArray, $temp_grade);
+            }
+
+            return $this->response($gradeArray);
+        } catch (\Exception $e) {
+            $data = [
+                'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                'errors' => $e->getMessage(),
+            ];
+
+            return $this->response($data, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    #[Route('/api/grade/{id}', name: 'update_grade', methods: ['PUT'])]
+    public function update_grade(Request $request, GradeRepository $gradeRepository, $id): Response
+    {
+        try {
+            $request = $this->transformJsonBody($request);
+
+            $grade = $gradeRepository->find($id);
+            $grade->setName($request->get('name'));
+            $grade->setDescription($request->get('description'));
+
+            $this->entityManager->persist($grade);
+            $this->entityManager->flush();
+
+            $data = [
+                'status' => Response::HTTP_OK,
+                'success' => 'Grade updated successfully',
+            ];
+
+            return $this->response($data);
+        } catch (\Exception $e) {
+            $data = [
+                'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                'errors' => $e->getMessage(),
+            ];
+            return $this->response($data, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    #[Route('/api/grade/{id}', name: 'delete_grade', methods: ['DELETE'])]
+    public function delete_grade(GradeRepository $gradeRepository, $id): Response
+    {
+        try {
+            $grade = $gradeRepository->find($id);
+
+            $this->entityManager->remove($grade);
+            $this->entityManager->flush();
+
+            $data = [
+                'status' => Response::HTTP_OK,
+                'success' => 'Grade deleted successfully',
+            ];
+
+            return $this->response($data);
+        } catch (\Exception $e) {
+            $data = [
+                'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                'errors' => $e->getMessage(),
+            ];
+            return $this->response($data, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+}
---
+++ api/src/DataFixtures/GradeFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Grade;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class GradeFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $grade1 = new Grade();
+        $grade1->setName('Junior Start');
+        $grade1->setDescription('Специалист, умеющий на базовом уровне работать с принятыми в проекте технологиями, но не имеющий за плечами ни «багажа» из пары лет опыта, ни знаний по текущему проекту.');
+        $manager->persist($grade1);
+
+        $grade2 = new Grade();
+        $grade2->setName('Middle');
+        $grade2->setDescription('Опытный сотрудник, готовый к самостоятельной работе.');
+        $manager->persist($grade2);
+
+        $manager->flush();
+    }
+}
