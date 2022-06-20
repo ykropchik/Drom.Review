a7b42c67c4724c56a6994288046d52b47714ba07
neu_net <przman2000@mail.ru>
2022-06-20 13:30:38 +1000

    BE-11 Увеличил максимальное количество символов для вопроса

--- api/src/Entity/Question.php
+++ api/src/Entity/Question.php
@@ -13,7 +13,7 @@ class Question
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 1024)]
     private $text;
 
     #[ORM\Column(type: 'array')]
