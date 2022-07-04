420c5c685fa2ee4f0f41da5f2dd3692d04ecfa68
neu_net <przman2000@mail.ru>
2022-07-04 15:56:33 +1000

    BE-14 Добавил получение всех ревью пользователя

--- api/src/Controller/StatisticController.php
+++ api/src/Controller/StatisticController.php
@@ -14,9 +14,8 @@ class StatisticController extends AppController
     public function get_statistic_respondent(UserRepository $userRepository, ReviewRepository $reviewRepository, $id): JsonResponse
     {
         try {
-            $user = $userRepository->find($id);
             $data = ['total' => 0, 'users' => []];
-            if (!$user) {
+            if (!$userRepository->find($id)) {
                 return $this->response([
                     'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
                     'message' => 'Пользователя с таким id не существует',
@@ -28,6 +27,34 @@ class StatisticController extends AppController
                 if ($review->isRespondent($id)) {
                     $data['total']++;
                     array_push($data['users'], ['user' => $review->getSubject(), 'specialization' => $review->getSpecialization(), 'grade' => $review->getGrade()]);
+                }
+            }
+
+            return $this->response($this->jsonSerialize($data));
+        } catch (\Exception $e) {
+            return $this->response([
+                'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                'message' => $e->getMessage(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    #[Route('/api/statistic/review/{id}', name: 'statistic_review', methods: ['GET'])]
+    public function get_statistic_review(UserRepository $userRepository, ReviewRepository $reviewRepository, $id): JsonResponse
+    {
+        try {
+            $data = [];
+            if (!$userRepository->find($id)) {
+                return $this->response([
+                    'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                    'message' => 'Пользователя с таким id не существует',
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            $reviews = $reviewRepository->findAll();
+            foreach ($reviews as $review) {
+                if ($review->getSubject()->getId() == $id) {
+                    array_push($data, $review);
                 }
             }
 
