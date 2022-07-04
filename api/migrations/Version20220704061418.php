a3330d92bcded80108ce07d2b88e12354bc617f9
neu_net <przman2000@mail.ru>
2022-07-04 18:12:19 +1000

    BE-14 Добавил получние статистики по ответам на вопросы ревью

--- api/migrations/Version20220704061418.php
+++ api/migrations/Version20220704061418.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220703073456 extends AbstractMigration
+final class Version20220704061418 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,12 +21,12 @@ final class Version20220703073456 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE grade (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE opinion (id INT AUTO_INCREMENT NOT NULL, question_id INT NOT NULL, estimate VARCHAR(255) NOT NULL, INDEX IDX_AB02B0271E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE opinion (id INT AUTO_INCREMENT NOT NULL, question_id INT NOT NULL, estimate VARCHAR(255) DEFAULT NULL, INDEX IDX_AB02B0271E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE qualification (id INT AUTO_INCREMENT NOT NULL, specialization_id INT DEFAULT NULL, grade_id INT DEFAULT NULL, INDEX IDX_B712F0CEFA846217 (specialization_id), INDEX IDX_B712F0CEFE19A1A8 (grade_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, specialization_id INT DEFAULT NULL, grade_id INT DEFAULT NULL, text VARCHAR(1024) NOT NULL, rating LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', INDEX IDX_B6F7494EFA846217 (specialization_id), INDEX IDX_B6F7494EFE19A1A8 (grade_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE respondent (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, question_id INT DEFAULT NULL, comment VARCHAR(255) DEFAULT NULL, status VARCHAR(20) NOT NULL, INDEX IDX_409B5150A76ED395 (user_id), INDEX IDX_409B51501E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE respondent (id INT AUTO_INCREMENT NOT NULL, review_id INT DEFAULT NULL, user_id INT DEFAULT NULL, comment VARCHAR(255) DEFAULT NULL, status VARCHAR(20) NOT NULL, INDEX IDX_409B51503E2E969B (review_id), INDEX IDX_409B5150A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE respondent_opinions (respondent_id INT NOT NULL, opinion_id INT NOT NULL, INDEX IDX_9926A734CE80CD19 (respondent_id), INDEX IDX_9926A73451885A6A (opinion_id), PRIMARY KEY(respondent_id, opinion_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, subject_id INT DEFAULT NULL, lead_id INT DEFAULT NULL, specialization_id INT DEFAULT NULL, grade_id INT DEFAULT NULL, date_start INT NOT NULL, date_end INT DEFAULT NULL, self_review VARCHAR(255) DEFAULT NULL, status VARCHAR(255) NOT NULL, history LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', INDEX IDX_794381C623EDC87 (subject_id), INDEX IDX_794381C655458D (lead_id), INDEX IDX_794381C6FA846217 (specialization_id), INDEX IDX_794381C6FE19A1A8 (grade_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE review_respondents (review_id INT NOT NULL, respondent_id INT NOT NULL, INDEX IDX_EE3E9D23E2E969B (review_id), INDEX IDX_EE3E9D2CE80CD19 (respondent_id), PRIMARY KEY(review_id, respondent_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE specialization (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE specialization_grades (specialization_id INT NOT NULL, grade_id INT NOT NULL, INDEX IDX_6D36A695FA846217 (specialization_id), INDEX IDX_6D36A695FE19A1A8 (grade_id), PRIMARY KEY(specialization_id, grade_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, full_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -36,14 +36,14 @@ final class Version20220703073456 extends AbstractMigration
         $this->addSql('ALTER TABLE qualification ADD CONSTRAINT FK_B712F0CEFE19A1A8 FOREIGN KEY (grade_id) REFERENCES grade (id)');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EFA846217 FOREIGN KEY (specialization_id) REFERENCES specialization (id)');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EFE19A1A8 FOREIGN KEY (grade_id) REFERENCES grade (id)');
+        $this->addSql('ALTER TABLE respondent ADD CONSTRAINT FK_409B51503E2E969B FOREIGN KEY (review_id) REFERENCES review (id)');
         $this->addSql('ALTER TABLE respondent ADD CONSTRAINT FK_409B5150A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE respondent ADD CONSTRAINT FK_409B51501E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
+        $this->addSql('ALTER TABLE respondent_opinions ADD CONSTRAINT FK_9926A734CE80CD19 FOREIGN KEY (respondent_id) REFERENCES respondent (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE respondent_opinions ADD CONSTRAINT FK_9926A73451885A6A FOREIGN KEY (opinion_id) REFERENCES opinion (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C623EDC87 FOREIGN KEY (subject_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C655458D FOREIGN KEY (lead_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6FA846217 FOREIGN KEY (specialization_id) REFERENCES specialization (id)');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6FE19A1A8 FOREIGN KEY (grade_id) REFERENCES grade (id)');
-        $this->addSql('ALTER TABLE review_respondents ADD CONSTRAINT FK_EE3E9D23E2E969B FOREIGN KEY (review_id) REFERENCES review (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE review_respondents ADD CONSTRAINT FK_EE3E9D2CE80CD19 FOREIGN KEY (respondent_id) REFERENCES respondent (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE specialization_grades ADD CONSTRAINT FK_6D36A695FA846217 FOREIGN KEY (specialization_id) REFERENCES specialization (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE specialization_grades ADD CONSTRAINT FK_6D36A695FE19A1A8 FOREIGN KEY (grade_id) REFERENCES grade (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_qualifications ADD CONSTRAINT FK_CE0ED15A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
@@ -57,11 +57,11 @@ final class Version20220703073456 extends AbstractMigration
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EFE19A1A8');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6FE19A1A8');
         $this->addSql('ALTER TABLE specialization_grades DROP FOREIGN KEY FK_6D36A695FE19A1A8');
+        $this->addSql('ALTER TABLE respondent_opinions DROP FOREIGN KEY FK_9926A73451885A6A');
         $this->addSql('ALTER TABLE user_qualifications DROP FOREIGN KEY FK_CE0ED151A75EE38');
         $this->addSql('ALTER TABLE opinion DROP FOREIGN KEY FK_AB02B0271E27F6BF');
-        $this->addSql('ALTER TABLE respondent DROP FOREIGN KEY FK_409B51501E27F6BF');
-        $this->addSql('ALTER TABLE review_respondents DROP FOREIGN KEY FK_EE3E9D2CE80CD19');
-        $this->addSql('ALTER TABLE review_respondents DROP FOREIGN KEY FK_EE3E9D23E2E969B');
+        $this->addSql('ALTER TABLE respondent_opinions DROP FOREIGN KEY FK_9926A734CE80CD19');
+        $this->addSql('ALTER TABLE respondent DROP FOREIGN KEY FK_409B51503E2E969B');
         $this->addSql('ALTER TABLE qualification DROP FOREIGN KEY FK_B712F0CEFA846217');
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EFA846217');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6FA846217');
@@ -75,8 +75,8 @@ final class Version20220703073456 extends AbstractMigration
         $this->addSql('DROP TABLE qualification');
         $this->addSql('DROP TABLE question');
         $this->addSql('DROP TABLE respondent');
+        $this->addSql('DROP TABLE respondent_opinions');
         $this->addSql('DROP TABLE review');
-        $this->addSql('DROP TABLE review_respondents');
         $this->addSql('DROP TABLE specialization');
         $this->addSql('DROP TABLE specialization_grades');
         $this->addSql('DROP TABLE user');
--- api/src/Controller/StatisticController.php
+++ api/src/Controller/StatisticController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionRepository;
 use App\Repository\ReviewRepository;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StatisticController extends AppController
 {
-    #[Route('/api/statistic/respondent/{id}', name: 'statistic_respondent', methods: ['GET'])]
+    #[Route('/api/statistic/review/{id}/respondent', name: 'statistic_respondent', methods: ['GET'])]
     public function get_statistic_respondent(UserRepository $userRepository, ReviewRepository $reviewRepository, $id): JsonResponse
     {
         try {
@@ -58,7 +59,48 @@ class StatisticController extends AppController
                 }
             }
 
-            return $this->response($this->jsonSerialize($data));
+            return $this->response($this->jsonSerialize($data, ['review-full', 'user-default', 'spec-default', 'grade-default', 'respondent-default']));
+        } catch (\Exception $e) {
+            return $this->response([
+                'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                'message' => $e->getMessage(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    #[Route('/api/statistic/review/{id}/opinions', name: 'statistic_review', methods: ['GET'])]
+    public function get_statistic_opinions(ReviewRepository $reviewRepository, QuestionRepository $questionRepository, $id): JsonResponse
+    {
+        try {
+            $review = $reviewRepository->find($id);
+            $data = [];
+            if (!$review) {
+                return $this->response([
+                    'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                    'message' => 'Ревью с таким id не существует',
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            $questions = $questionRepository->findBy(['specialization' => $review->getSpecialization()->getId(), 'grade' => $review->getGrade()->getId()]);
+
+            foreach ($questions as $question) {
+                $statistic = [];
+                foreach ($question->getRating() as $estimate) {
+                    array_push($statistic, ['answer' => $estimate, 'count' => 0]);
+                }
+                array_push($data, ['id' => $question->getId(), 'text' => $question->getText(), 'statistic' => $statistic]);
+            }
+
+            $respondents = $review->getRespondents();
+            foreach ($respondents as $respondent) {
+                $opinions = $respondent->getOpinions();
+                foreach ($opinions as $opinion) {
+                    $key = array_search($opinion->getQuestion()->getId(), array_column($data, 'id'));
+                    $data[$key]['statistic'][array_search($opinion->getEstimate(), array_column($data[$key]['statistic'], 'answer'))]['count']++;
+                }
+            }
+
+            return $this->response($data);
         } catch (\Exception $e) {
             return $this->response([
                 'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
