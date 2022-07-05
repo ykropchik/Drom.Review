03aaec006b0813940be5e73c9fb0e1c20314aa29
ykropchik <ykropchik22@gmail.com>
2022-07-05 10:57:02 +1000

    Backend Новые фикстуры

--- api/src/DataFixtures/GradeFixtures.php
+++ api/src/DataFixtures/GradeFixtures.php
@@ -8,19 +8,35 @@ use Doctrine\Persistence\ObjectManager;
 
 class GradeFixtures extends Fixture
 {
+	public const GRADES = [
+		'grade1' => 'Junior Start',
+		'grade2' => 'Junior Full',
+		'grade3' => 'Middle Start',
+		'grade4' => 'Middle Progressive',
+		'grade5' => 'Middle Full',
+		'grade6' => 'Senior Start',
+		'grade7' => 'Senior Full',
+	];
+
+	public const GRADES_DESCRIPTION = [
+		'grade1' => 'Способен самостоятельно писать свой код, понимает принципы работы программ, может разобраться в чужом коде, успешно справляется с простыми задачами, а при помощи ментора может принять участие и в решении средних по сложности задач.',
+		'grade2' => 'Имеет определнный опыт достаточный для того, чтобы выполнять какие-то задачи самостоятельно. Требует меньше контроля со стороны наставника',
+		'grade3' => 'Разработчик, способный самостоятельно выполнять сложные задачи. Имеет достаточно опыта и знаний, чтобы делать ревью и проверять код на ошибки.',
+		'grade4' => 'Способен работать в команде, принимая участие в решении задач. Понимает как решать бизнес задачи.',
+		'grade5' => 'Обладает хороший уровень soft скиллов, может взять на себя роль наставника.',
+		'grade6' => 'Разработчик имеет сильные hard скиллы, может с легкостью добавить новый функционал в проект. Активно влияет на процесс приоритезации задач, а также активно взаимодействует с другими командами (в том числе командами мэнеджеров, аналитиков и т.д.)',
+		'grade7' => 'Обладает сильными soft скиллами, активно влияет на атмосферу в команде, руководит процессов разработки.',
+	];
+
     public function load(ObjectManager $manager): void
     {
-        $grade1 = new Grade();
-        $grade1->setName('Junior - Start');
-        $grade1->setDescription('Специалист, умеющий на базовом уровне работать с принятыми в проекте технологиями, но не имеющий за плечами ни «багажа» из пары лет опыта, ни знаний по текущему проекту.');
-        $manager->persist($grade1);
-		$this->addReference('grade1', $grade1);
-
-        $grade2 = new Grade();
-        $grade2->setName('Middle - Full');
-        $grade2->setDescription('Опытный сотрудник, готовый к самостоятельной работе.');
-        $manager->persist($grade2);
-		$this->addReference('grade2', $grade2);
+		foreach (self::GRADES as $key => $value) {
+			$grade = new Grade();
+			$grade->setName($value);
+			$grade->setDescription(self::GRADES_DESCRIPTION[$key]);
+			$manager->persist($grade);
+			$this->addReference($key, $grade);
+		}
 
         $manager->flush();
     }
--- api/src/DataFixtures/SpecializationFixtures.php
+++ api/src/DataFixtures/SpecializationFixtures.php
@@ -12,13 +12,26 @@ class SpecializationFixtures extends Fixture
     {
         $specialization1 = new Specialization();
         $specialization1->setName('Frontend');
-		$specialization1->setGrades([$this->getReference('grade1'), $this->getReference('grade2')]);
+		$specialization1->setGrades([
+			$this->getReference('grade1'),
+			$this->getReference('grade2'),
+			$this->getReference('grade3'),
+			$this->getReference('grade4'),
+			$this->getReference('grade5'),
+			$this->getReference('grade7'),
+		]);
         $manager->persist($specialization1);
 		$this->addReference('specialization1', $specialization1);
 
         $specialization2 = new Specialization();
         $specialization2->setName('Backend');
-	    $specialization2->setGrades([$this->getReference('grade1'), $this->getReference('grade2')]);
+	    $specialization2->setGrades([
+		    $this->getReference('grade1'),
+		    $this->getReference('grade2'),
+		    $this->getReference('grade3'),
+		    $this->getReference('grade5'),
+		    $this->getReference('grade6'),
+	    ]);
         $manager->persist($specialization2);
 		$this->addReference('specialization2', $specialization2);
 
